1f55d16a5e4228a38c1d3c3100ced21e0c22cecf
Tristan Sizik <tristansizik@gmail.com>
2019-03-24 19:38:03 -0700

    Fixing

--- apiKeys.js
+++ apiKeys.js
@@ -1,8 +1,8 @@
 module.exports = {
-    consumer_key: "process.env.BOT_CONSUMER_KEY",
-    consumer_secret: "process.env.BOT_CONSUMER_SECRET",
-    access_token: "process.env.BOT_ACCESS_TOKEN",
-    access_token_secret: "process.env.BOT_ACCESS_TOKEN_SECRET"
+    consumer_key: process.env.BOT_CONSUMER_KEY,
+    consumer_secret: process.env.BOT_CONSUMER_SECRET,
+    access_token: process.env.BOT_ACCESS_TOKEN,
+    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 }
 //Set these api keys to be hardcoded when developing, but set them to this when on heroku
 //enter api keys on heroku dashboard
